147d4bcc99f2fb98875a7b4eecec84ef67f678f3
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-29 21:52:11 -0600

    commit tree d9b0546a5d033ca291590d5f69e1cafd0acbadd2

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    #binding.pry
+    delimiters = ['...','?','!','!!','..','.']
+    self.split(/[[!?.]]\s/).count
+    #how is this done without Regex????????
+     #HOW DO I DISAPPEAR ("")?
+    ##delete("") quick-fix - cheater
+    #["This, well, is a sentence", " This is too", "", " And so is this, I think", " Woo"]
+    end
 end
